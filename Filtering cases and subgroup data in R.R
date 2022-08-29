425277ac175e770271e86e967ddc5eeef7902558
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-08-29 03:06:08 -0400

    Add files via upload

---
+++ Filtering cases and subgroup data in R.R
@@ -0,0 +1,34 @@
+# Filtering cases and subgroup data in R  
+### make sure to install all the packages below. 
+install.packages("tidyverse")
+pacman::p_load(pacman, tidyverse, rio)
+
+data <- import("stateData.xlsx") %>% as_tibble()%>%
+  select(state_code, region, psychRegions, instagram:modernDance) %>%
+  print()
+
+view(data)  
+
+attach(data)
+### histogram for entrepreneur
+qplot(entrepreneur, geom = "histogram", data = data)
+
+###  filtering entrepreneur from above 1
+data1 <- data %>% filter(entrepreneur > 1) %>% print()
+view(data1)
+
+### Region is  character variable 
+qplot(region, data  = data)
+
+#### looking for th states in the south 
+data %>% filter(region == "South")
+
+### looking for all the states whose psyvhregion is Relaxed and creative 
+qplot(psychRegions, data = data)
+data %>% filter(psychRegions == "Relaxed and Creative") %>% print()
+
+
+### filtering by and or and not 
+data %>% filter(region == "North" | psychRegions == "Friendly and Conventional")
+data %>% filter(region == "South" & psychRegions == "Relaxed and Creative")
+data %>% filter(!region == "South" & psychRegions == "Relaxed and Creative")
